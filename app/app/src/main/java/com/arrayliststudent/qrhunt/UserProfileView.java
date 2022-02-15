f150bc5ad3655d657bdd40b44504691adeadc9a3
Int-0X7FFFFFFF <563748846@qq.com>
2022-02-15 21:50:03 +0000

    New Layout

---
+++ app/app/src/main/java/com/arrayliststudent/qrhunt/UserProfileView.java
@@ -0,0 +1,14 @@
+package com.arrayliststudent.qrhunt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserProfileView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile_view);
+    }
+}
