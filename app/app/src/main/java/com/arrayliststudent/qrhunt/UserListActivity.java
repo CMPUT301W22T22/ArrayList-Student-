04da5efb45171af3962ed2641824696575c8d293
Intilyc <jman0163@gmail.com>
2022-02-18 15:24:08 -0700

    Added the user and code list activities. XML included.

--- app/app/src/main/java/com/arrayliststudent/qrhunt/UserListActivity.java
+++ app/app/src/main/java/com/arrayliststudent/qrhunt/UserListActivity.java
@@ -1,14 +1,13 @@
 package com.arrayliststudent.qrhunt;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class HighScoreListView extends AppCompatActivity {
+public class UserListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_high_score_list_view);
+        setContentView(R.layout.activity_user_list);
     }
 }
