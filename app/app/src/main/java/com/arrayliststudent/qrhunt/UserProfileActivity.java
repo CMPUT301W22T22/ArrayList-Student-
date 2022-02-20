8ccb2c3816cde871d96a22aebf87df4fe16e2dfd
0X7FFFFFFF <31471833+Int-0X7FFFFFFF@users.noreply.github.com>
2022-02-20 16:36:53 -0700

    Revert "Jiemin wang"

--- app/app/src/main/java/com/arrayliststudent/qrhunt/UserProfileActivity.java
+++ app/app/src/main/java/com/arrayliststudent/qrhunt/UserProfileActivity.java
@@ -3,26 +3,12 @@ package com.arrayliststudent.qrhunt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class UserProfileActivity extends AppCompatActivity {
-
-    private User user;
-    private TextView userName;
-    private TextView contactInfo;
-    private TextView score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
-        userName = findViewById(R.id.UserName);
-        contactInfo = findViewById(R.id.ContactInfo);
-        score = findViewById(R.id.ContactInfo);
-        userName.setText(user.getUserName());
-        contactInfo.setText(String.valueOf(user.getContactInfo()));
-        score.setText(String.valueOf(user.getScore()));
     }
-
-
 }
