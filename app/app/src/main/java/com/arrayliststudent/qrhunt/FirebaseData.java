59e3cf1e52f9dc687dc233e5e5aa37a1bd519b9a
Intilyc <44782050+Intilyc@users.noreply.github.com>
2022-02-25 16:42:20 -0700

    Added User data control to FirebaseData.

--- app/app/src/main/java/com/arrayliststudent/qrhunt/FirebaseData.java
+++ app/app/src/main/java/com/arrayliststudent/qrhunt/FirebaseData.java
@@ -1,14 +1,58 @@
 package com.arrayliststudent.qrhunt;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-//Firebase Docs: https://firebase.google.com/docs/guides?authuser=0&hl=en
-//Firebase Samples: https://firebase.google.com/docs/samples?authuser=0&hl=en
-
+/**
+ * Firebase controller; responsible for getting and setting Firebase data
+ *
+ * Built using the lab guide at: https://github.com/AbdulAli/FirestoreTutorialW22/
+ * Firebase Docs: https://firebase.google.com/docs/guides?authuser=0&hl=en
+ * Firebase Samples: https://firebase.google.com/docs/samples?authuser=0&hl=en
+ *
+ * @author jmgraham
+ * @see User
+**/
 public class FirebaseData {
     final String TAG = "you're it";
     FirebaseFirestore database;
 
+    public FirebaseData(User user){//Constructor for user data
+    }
+
+    public FirebaseData(){//Constructor for Code data
+        database = FirebaseFirestore.getInstance();
+        final CollectionReference collectionReference = database.collection("Codes");
+    }
+
+    public void addUserData(User user){
+        database = FirebaseFirestore.getInstance();
+        final CollectionReference collectionReference = database.collection("Users");
+
+        collectionReference
+                .document(String.valueOf(user.hashCode()))
+                .set(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        //if data is successfully uploaded
+                        Log.d(TAG, "User data successfully uploaded");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //if data upload fails
+                        Log.d(TAG, "User data failed to upload: " + e.toString());
+                    }
+                });
+    }
 }
 
 /*
