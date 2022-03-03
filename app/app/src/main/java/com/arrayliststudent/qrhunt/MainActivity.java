2164a21157302b5d7dbb65126a06d53db987da6d
Haoyu Zhang <haoyu16@ualberta.ca>
2022-03-02 18:31:10 -0700

    I added my geolocation API into my branch

--- app/app/src/main/java/com/arrayliststudent/qrhunt/MainActivity.java
+++ app/app/src/main/java/com/arrayliststudent/qrhunt/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_console);
-        if(auth.login(Settings.Secure.ANDROID_ID)) {
-        }
+        /*if(auth.login(Settings.Secure.ANDROID_ID)) {
+        }*/
     }
 }
