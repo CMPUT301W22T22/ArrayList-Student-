aa9aa32ad67cd9314ad75bcf4aabe0778cc0087c
Intilyc <jman0163@gmail.com>
2022-03-12 20:02:06 -0700

    Implemented touch listeners in ListActivities

--- app/src/main/java/com/arrayliststudent/qrhunt/UserListActivity.java
+++ app/src/main/java/com/arrayliststudent/qrhunt/UserListActivity.java
@@ -8,20 +8,46 @@ import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.util.HashMap;
+
 public class UserListActivity extends AppCompatActivity {
     private RecyclerView userList;
     private TextView totalUserBase;
     private TextInputEditText searchbar;
 
+    private CustomRVAdapter userListAdapter;
+    private HashMap<Integer, User> userDataList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list);
+        userDataList = UserDataModel.getInstance().userList;
+
+        userList.addOnItemTouchListener(new RecyclerViewTou(getApplicationContext(), userList, new RecyclerViewClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Toast.makeText(getApplicationContext(), bookList.get(position).getTitle() + " is clicked!", Toast.LENGTH_SHORT).show();
+            }
+        }));
 
         userList = findViewById(R.id.user_list_view);
         totalUserBase = findViewById(R.id.total_userbase_view);
         searchbar = findViewById(R.id.search_bar);
+
+
+        userListAdapter = new UserListAdapter();
+        totalUserBase.setText(userDataList.size());
+        userList.setAdapter(UserListAdapter);
     }
 
+    /*
+    recyclerView.addOnItemTouchListener(new RecyclerViewTouchListener(getApplicationContext(), recyclerView, new RecyclerViewClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Toast.makeText(getApplicationContext(), bookList.get(position).getTitle() + " is clicked!", Toast.LENGTH_SHORT).show();
+            }
+        }));
+     */
 
 }
