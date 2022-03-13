2b15b5ed925063f44391a675b8ce2b8c0fce41b5
Kieran <na>
2022-03-12 19:44:38 -0700

    Added 'Add Comment' Button and Fragment

---
+++ app/src/main/java/com/arrayliststudent/qrhunt/AddCommentFragment.java
@@ -0,0 +1,59 @@
+package com.arrayliststudent.qrhunt;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+
+public class AddCommentFragment extends DialogFragment {
+
+    private EditText addComment;
+    private OnFragmentInteractionListener listener;
+
+    public interface OnFragmentInteractionListener{
+        void onCommentOkPressed(String commentBody);
+    }
+
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+        if (context instanceof AddCommentFragment.OnFragmentInteractionListener)
+        {
+            listener = (AddCommentFragment.OnFragmentInteractionListener) context;
+        }
+        else
+        {
+            throw new RuntimeException(context.toString()
+                    +"must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    @NonNull
+    public Dialog onCreateDialog(@NonNull Bundle savedInstanceState){
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_add_comment,null);
+
+        addComment = view.findViewById(R.id.comment_frag);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        return builder
+                .setView(view)
+                .setTitle("Add Comment")
+                .setNegativeButton("Cancel",null)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String commentBody = addComment.getText().toString();
+                        listener.onCommentOkPressed(commentBody);
+                    }
+                }).create();
+
+    }
+}
--- app/src/main/java/com/arrayliststudent/qrhunt/CommentsActivity.java
+++ app/src/main/java/com/arrayliststudent/qrhunt/CommentsActivity.java
@@ -1,17 +1,27 @@
 package com.arrayliststudent.qrhunt;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
-public class CommentsActivity extends AppCompatActivity {
+public class CommentsActivity extends AppCompatActivity implements AddCommentFragment.OnFragmentInteractionListener{
 
     ListView commentView;
     ArrayList<Comment> comments;
     CommentAdapter commentAdapter;
+
+    private View.OnClickListener onACButtonClicked = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //launch add comment fragment
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +32,23 @@ public class CommentsActivity extends AppCompatActivity {
         //get list of comments
 
         //create adapter
-        commentAdapter = new CommentAdapter(this, comments);
+
+        //commentAdapter = new CommentAdapter(this, comments);
+
         //tie view to adapter
-        commentView.setAdapter(commentAdapter);
+
+        //commentView.setAdapter(commentAdapter);
+
         //attach list to view
-        commentView = findViewById(R.id.comment_list);
         //display list of comments
+
+        final Button addCommentButton = findViewById(R.id.add_comment_butt);
+        addCommentButton.setOnClickListener((v) ->
+                new AddCommentFragment().show(getSupportFragmentManager(), "ADD_COMMENT"));
     }
 
+    @Override
+    public void onCommentOkPressed(String comment) {
+        //add to list of comments (need author)
+    }
 }
